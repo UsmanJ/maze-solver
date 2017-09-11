2a488541285a7d95eaa88029cdd79e764bef8286
Usman Jamil <usman.jamil@live.co.uk>
2017-09-12 00:19:57 +0100

    Changed App and NotFound to pure functions

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import css from './style.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className={css.app}>
-        <div className={css.header}>
-          <img src={logo} className={css.logo} alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className={css.intro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className={css.app}>
+    <div className={css.header}>
+      <img src={logo} className={css.logo} alt="logo" />
+      <h2>Welcome to React</h2>
+    </div>
+    <p className={css.intro}>
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
 
 export default App;
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,24 +1,17 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import classnames from 'classnames';
 
 // import './style.css';
 
-export default class NotFound extends Component {
-  render() {
-    const { className, ...props } = this.props;
-    return (
-      <div className={classnames('NotFound', className)} {...props}>
-        <h1>
-          404 <small>Not Found :(</small>
-        </h1>
-      </div>
-    );
-  }
-}
-
-NotFound.propTypes = {
-  className: PropTypes.string,
+const NotFound = (props) => {
+  const { className } = this.props;
+  return (
+    <div className={classnames('NotFound', className)} {...props}>
+      <h1>
+        404 <small>Not Found :(</small>
+      </h1>
+    </div>
+  );
 };
 
 NotFound.defaultProps = { className: '' };
